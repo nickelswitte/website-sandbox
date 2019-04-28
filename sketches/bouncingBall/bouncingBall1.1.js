a719aee69e5c812131c29c3330f6918d5a9fb4e4
nickelswitte <witte.nickels-tim17@it.dhbw-ravensburg.de>
2019-04-28 12:40:42 +0200

    Added new version of BouncingBall: 1.1

--- sketches/bouncingBall/bouncingBall1.1.js
+++ sketches/bouncingBall/bouncingBall1.1.js
@@ -4,7 +4,7 @@
  * screen.
  * 
  * Author:  Nickels Witte
- * Date:    27.04.2019
+ * Date:    28.04.2019
  * Version: 1.1
  */
 const bouncingBallConst = (p) => {
@@ -13,8 +13,9 @@ const bouncingBallConst = (p) => {
     let x;
     let y;
 
-    //Other properties of the ball
+    //variable that holds the speed of the circle
     let speed = 5;
+    let speedBackup = speed;
 
     /**
      * These are the vector components of the circle
@@ -39,6 +40,8 @@ const bouncingBallConst = (p) => {
     //size of the circle
     let circleWidth = 50;
 
+    //Stores if it should follow the mouse at the moment
+    let followMouse = false;
 
     //This will be calles on start
     p.setup = function() {
@@ -73,8 +76,14 @@ const bouncingBallConst = (p) => {
         p.background(127);
 
         //Make the fill color for the ball white
-        p.fill(255);
+        if (followMouse) {
+            p.fill('#f25f5c');
+        } else {
+            p.fill(255);
+        }
+        
     
+        //check for collision with walls and change direction
         p.checkForCollision(0, p.width, 0, p.height);
 
         //Moving x, depending on direction
@@ -86,7 +95,8 @@ const bouncingBallConst = (p) => {
         
         p.printInformation(10, 20);
 
-        //p.setDirectionTo(p.mouseX, p.mouseY);
+        //chance direction every frame so it follows all the time
+        p.attemptToFollowMouse();
     };
 
 
@@ -133,25 +143,95 @@ const bouncingBallConst = (p) => {
         {
             startedCorrectionY = false;    
         }
-    }
+
+        /**
+         * This part will check for collision with the mouse when the follow
+         * mouse mode is activated. When there is a collision, it deactivates
+         * the following, as it would otherwise shake under the mouse
+         */
+        if (
+            (Math.abs(x - p.mouseX) < 5) &&
+            (Math.abs(y - p.mouseY) < 5) &&
+            followMouse
+        ) {
+            followMouse = false;
+        }
+    };
+
+    /**
+     * This method will check if the followMouse variable is set
+     * to true and if so change the direction to that coordinate.
+     */
+    p.attemptToFollowMouse = function() {
+        if (!followMouse) {
+            return;
+        }
+
+        //Only do this when inside the canvas
+        if (
+            (p.mouseX >= 0 && p.mouseX <= p.width) &&
+            (p.mouseY >= 0 && p.mouseY <= p.height)
+        ) {
+            p.setDirectionTo(p.mouseX, p.mouseY);
+        }
+    };
 
     /**
      * This method prints information about the circle
      */
     p.printInformation = function(textX, textY) {
+        // First save the current draw settings
+        p.push();
+
+        p.fill(255);
+
         let textLineWidth = 15;
 
         p.text('X: ' + Math.round(x), textX, textY);
 
         p.text('Y: ' + Math.round(y), textX, textY + textLineWidth);
 
-        p.text('vectorX: ' + vectorX, textX, textY + 2 * textLineWidth);
+        p.text('speed: ' + speed, textX, textY + 2 * textLineWidth);
 
-        p.text('vectorY: ' + vectorY, textX, textY + 3 * textLineWidth);
+        p.text('Click or press Space', textX, textY + 3 * textLineWidth);
 
-        p.text('speed: ' + speed, textX, textY + 4 * textLineWidth);
 
-        p.text('Things to do: Click, K, L', textX, textY + 5 * textLineWidth);
+        /**
+         * Small visual display of the current circle vector
+         */
+        // and change them to what we need
+        p.stroke(255);
+        p.noFill();
+
+        // The coordinates of the center of the display relative to the
+        // coordinates of the information
+        let vectorDisplayCenterX = textX + 80;
+        let vectorDisplayCenterY = textY + 5;
+
+        // Length of line and circle radius
+        let vectorDisplayLength = 15;
+
+        // Calculation of the end points of the vector needs to be done
+        let vectorEndX = vectorDisplayCenterX + vectorX * vectorDisplayLength;
+        let vectorEndY = vectorDisplayCenterY + vectorY * vectorDisplayLength;
+
+        // Draw the line of the vector
+        p.line(
+            vectorDisplayCenterX, 
+            vectorDisplayCenterY, 
+            vectorEndX, 
+            vectorEndY
+        );
+        
+        // Draw the ellipse as some kind of border
+        p.ellipse(
+            vectorDisplayCenterX, 
+            vectorDisplayCenterY, 
+            vectorDisplayLength * 1.8
+        );
+
+        // Delete the changed draw settings.
+        p.pop();
     };
 
 
@@ -174,23 +254,27 @@ const bouncingBallConst = (p) => {
 
     //Change direction on mouse click
     p.mousePressed = function() {
-
-        //only cound clicks inside the canvas
         if (
             (p.mouseX >= 0 && p.mouseX <= p.width) &&
             (p.mouseY >= 0 && p.mouseY <= p.height)
         ) {
-            p.setDirectionTo(p.mouseX, p.mouseY);
+            followMouse = (followMouse == false); 
         }
-        
     };
 
-    //On keys "K" and "L" change speeds
+    /**
+     * This method is called whenever there is a key pressed
+     */
     p.keyPressed = function() {
-        if (p.key == 'k') {
-            speed = 0;
-        } else if (p.key == 'l') {
-            speed = 3;
+
+        if (p.key == ' ') {
+            // Toggle mechanism for pausing the circle
+            if (speed != 0) {
+                speedBackup = speed;
+                speed = 0;
+            } else {
+                speed = speedBackup;
+            }
         }
       }
 
