ae1944ea0fe7979a6e8fd723160964f4a86f9e65
nickelswitte <witte.nickels-tim17@it.dhbw-ravensburg.de>
2020-02-15 20:30:56 +0100

    Finished template to be nice

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# website-sandbox
-`under development`
+# Sketches Website
 
-This is the branch `database` that will be used to develop a database backend to the website
+This is the branch `database` that will be used to develop a database backend to the website.
+
+## Creating a new sketch
+1. Copy the file `template.js` into a new folder under `./sketches`.
+2. Create a new unique id for that sketch and and replace the template at the top.
+3. Add the database entry with the unique_id as `variableName`.
+
--- sketches/template.js
+++ sketches/template.js
@@ -1,52 +1,78 @@
 
 /**
- * This is a placeholder
+ * This is a template for a p5 sketch used in the sketches website by nickels witte.
+ * 
+ * !!! Replace all occurences of the ID below with a unique id !!!
+ * UNIQUE_ID = REPLACE_THIS
  * 
  * Author:  Nickels Witte
- * Date:    13.2.2020
+ * Date:    
  * Version  1.0
  */
-const placeHolderConst = (p) => {
+const REPLACE_THISConst = (p) => {
     
     // Setup function
     p.setup = function() {
 
-        var c = p.createCanvas(800, 400);
+        // Create the canvas and put it inside the parent div
+        var c = p.createCanvas();
+        c.parent(REPLACE_THIS.sketchDivId);
+        // Call this method to get it to right size
         p.windowResized();
-        c.parent("#1005");
 
+        // Example button
         button = p.createButton('click me');
         button.addClass('btn btn-secondary sketchControl');
-        button.parent('7001controlsDivName');
+        button.parent(REPLACE_THIS.controlsDivId);
 
     };
 
+    /**
+     * This function is called at the creation of the sketch 
+     * and whenever the windowsize is changing.
+     * It takes care to always resize the canvas to the right size.
+     */
     p.windowResized = function()  {
-        let div = p.select('#7001').size();
+        // Find the parent div and get its size
+        let div = p.select(REPLACE_THIS.sketchDivId).size();
+
+        // Resize canvas
         p.resizeCanvas(div.width, div.width / 2);
-        p.resetSketch();
+
+        // If wished, reset the sketch
+        // p.resetSketch();
     }
 
-    //Function to reset the sketch back to normal
+    /**
+     * Function to bring the sketch back to a certain state
+     */
     p.resetSketch = function() {
-
+        // do stuff
     };
 
+    /**
+     * This method is looped by p5
+     * Here some action can be done
+     */
     p.draw = function() {
-        
+        // Do something to make it move
     }
 
+    /**
+     * Function is called when mouse is pressed
+     */
     p.mousePressed = function() {
+        // Check if it is pressed inside the sketch
         if (
             (p.mouseX >= 0 && p.mouseX <= p.width) &&
             (p.mouseY >= 0 && p.mouseY <= p.height)
         ) {
-            
+            // Do something
         }
         
     };
 
 };
 
-//Creating the object
-let ph_a = new p5(placeHolderConst);
+//Creating the sketch object from the constant
+let REPLACE_THISSketch = new p5(REPLACE_THISConst);
